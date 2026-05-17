7022913ad77eb27aae7d60246fdd558fba466f30
Gaizka <chocarrovaz.gaizka@gmail.com>
2026-05-17 13:23:33 +0200

    Finalizada pagina index

--- src/routes/publicRoutes.js
+++ src/routes/publicRoutes.js
@@ -6,7 +6,7 @@ const router=express.Router()
 //////////////////////////////////////////////////////////////////////////////////////
 
 router.get('/', (req, res) => {
-    res.send("<h1>Práctica JWT</h1>");
+    res.render("index");
 })
 
 
